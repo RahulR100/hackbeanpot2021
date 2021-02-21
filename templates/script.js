a72379dd1e20dc606d82d5c92a84b851be1d4803
jaron-cui <jaron.cui@gmail.com>
2021-02-20 21:06:19 -0500

    merge progress!

--- templates/script.js
+++ templates/script.js
@@ -1,5 +1,77 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiZGFuc3dpY2siLCJhIjoiY2l1dTUzcmgxMDJ0djJ0b2VhY2sxNXBiMyJ9.25Qs4HNEkHubd4_Awbd8Og';
+mapboxgl.accessToken =
+        'pk.eyJ1IjoiY2x1ZWxlc3M0IiwiYSI6ImNrbGQ5d2hwcjBxNGsydm9iMW9saXAxZW0ifQ.9sZd8Fc8ojC_ycnEHXNZ_Q';
 
+navigator.geolocation.getCurrentPosition(successLocation,
+        errorLocation, { enableHighAccuracy: true
+      })
+
+
+      function errorLocation() {
+        setupMap([0,0])
+
+      }
+
+      function successLocation(position) {
+        setupMap([position.coords.longitude, position.coords.latitude]); // northeastern
+      }
+
+      function setupMap(center) {
+        var map = new mapboxgl.Map({
+          container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: center,
+            zoom: 14
+        })
+
+        // my location
+        var myLocation = new mapboxgl.Marker()
+          .setLngLat(center)
+          .addTo(map);
+
+        // data points (hardcoded rn)
+        var geojson = {
+          type: 'FeatureCollection',
+          features: [{
+            type: 'Feature',
+            geometry: {
+              type: 'Point',
+              coordinates: [-71.0875, 42.3398]
+            },
+            properties: {
+              title: 'Northeastern',
+              description: 'Virtual Quad ;)'
+            }
+          },
+          {
+            type: 'Feature',
+            geometry: {
+              type: 'Point',
+              coordinates: [-71.1049, 42.3374]
+            },
+            properties: {
+              title: 'Boston Children Hospital',
+              description: 'For the children'
+            }
+          }]
+        }
+
+        // add markers to map
+        geojson.features.forEach(function(marker) {
+
+          // create a HTML element for each feature
+          var el = document.createElement('div');
+          el.className = 'marker';
+
+          // make a marker for each feature and add to the map
+          new mapboxgl.Marker(el)
+            .setLngLat(marker.geometry.coordinates)
+            .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+            .setHTML('<h3>' + marker.properties.title + '</h3><p>'
+              + marker.properties.description + '</p>'))
+            .addTo(map)
+        })
+      }
+/*
 var map = new mapboxgl.Map({
     container: 'map', // container id
     style: 'mapbox://styles/mapbox/streets-v9', //stylesheet location
@@ -34,7 +106,7 @@ map.on('load', function() {
         map.resize();
     }
 
-});
+});*/
 //{title, userdets, image, quantity, price, location, description, tags}
 const items = [{title: "apple", location: 1, quantity: 2}, {title: "toilet paper", location: 0, quantity: 6}]
 
