1f6b8c96807b4125691aaea558ac6523fee04485
jaron-cui <jaron.cui@gmail.com>
2021-02-20 21:57:54 -0500

    pins now respond to pain

--- templates/script.js
+++ templates/script.js
@@ -1,30 +1,5 @@
-mapboxgl.accessToken =
-        'pk.eyJ1IjoiY2x1ZWxlc3M0IiwiYSI6ImNrbGQ5d2hwcjBxNGsydm9iMW9saXAxZW0ifQ.9sZd8Fc8ojC_ycnEHXNZ_Q';
 
-navigator.geolocation.getCurrentPosition(successLocation,
-    errorLocation, { enableHighAccuracy: true
-  })
-
-function errorLocation() {
-  setupMap([0,0])
-}
-
-function successLocation(position) {
-  setupMap([position.coords.longitude, position.coords.latitude]); // northeastern
-}
-
-function setupMap(center) {
-  var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: center,
-    zoom: 14
-  })
-
-  // my location
-  var myLocation = new mapboxgl.Marker().setLngLat(center).addTo(map);
-
-  // data points (hardcoded rn)
+// data points (hardcoded rn)
   var geojson = {
     type: 'FeatureCollection',
     features: [{
@@ -50,22 +25,54 @@ function setupMap(center) {
     }]
   }
 
-        // add markers to map
-        geojson.features.forEach(function(marker) {
 
-          // create a HTML element for each feature
-          var el = document.createElement('div');
-          el.className = 'marker';
+mapboxgl.accessToken =
+        'pk.eyJ1IjoiY2x1ZWxlc3M0IiwiYSI6ImNrbGQ5d2hwcjBxNGsydm9iMW9saXAxZW0ifQ.9sZd8Fc8ojC_ycnEHXNZ_Q';
+var map;
+navigator.geolocation.getCurrentPosition(successLocation,
+    errorLocation, { enableHighAccuracy: true
+  })
 
-          // make a marker for each feature and add to the map
-          new mapboxgl.Marker(el)
-            .setLngLat(marker.geometry.coordinates)
-            .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-            .setHTML('<h3>' + marker.properties.title + '</h3><p>'
-              + marker.properties.description + '</p>'))
-            .addTo(map)
-        })
-      }
+function errorLocation() {
+  setupMap([0,0])
+}
+
+function successLocation(position) {
+  setupMap([position.coords.longitude, position.coords.latitude]); // northeastern
+}
+
+function setupMap(center) {
+  map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: center,
+    zoom: 14
+  })
+
+  // my location
+  var myLocation = new mapboxgl.Marker().setLngLat(center).addTo(map);
+
+  // add markers to map
+  geojson.features.forEach(addMarker);
+}
+
+function addMarker(marker) {
+  // create a HTML element for each feature
+  var el = document.createElement('div');
+  el.className = 'marker';
+
+  // make a marker for each feature and add to the map
+  const pin = new mapboxgl.Marker(el)
+    .setLngLat(marker.geometry.coordinates)
+    .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+    .setHTML('<h3>' + marker.properties.title + '</h3><p>'
+        + marker.properties.description + '</p>'))
+    .addTo(map);
+
+  pin.getElement().addEventListener('click', () => {
+    alert("Clicked");
+  });
+}
 /*
 var map = new mapboxgl.Map({
     container: 'map', // container id
