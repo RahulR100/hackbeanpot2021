367c0b263cbd1ca1ea3a11c2e602f8115c7d1c6d
RahulR100 <rahulrao@email.com>
2021-02-20 22:07:29 -0500

    fixed user validation

--- products/views.py
+++ products/views.py
@@ -3,4 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
+	context = {
+		'login_success': False if request.session.get('login_success') is None else request.session.get('login_success'),
+	}
+
+	if request.session.get('login_success') is not None:
+		del request.session['login_success']
+
 	return render(request, 'home.html', {})
