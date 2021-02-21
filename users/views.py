be3eff7c995f305b635025a800d0780137dcbfc4
RahulR100 <rahulrao@email.com>
2021-02-20 19:24:48 -0500

    changed pyc

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.hashers import make_password
 
 from .forms import UserRegisterForm
 from .models import User
@@ -7,11 +8,25 @@ from .models import User
 def register_view(request, *args, **kwargs):
 	form = UserRegisterForm(request.POST or None)
 	if form.is_valid():
-		form.save()
+		username = request.POST.get('username')
+		first_name = request.POST.get('first_name')
+		last_name = request.POST.get('last_name')
+		password = make_password(request.POST.get('password'))
+		email = request.POST.get('email')
+		phone_number = request.POST.get('phone_number')
+		address = request.POST.get('address')
+		is_seller = False if request.POST.get('is_seller') == None else True
 
-	context = {'form': form}
+		User.objects.create(username=username,
+							first_name=first_name,
+							last_name=last_name,
+							password=password,
+							email=email,
+							phone_number=phone_number,
+							address=address,
+							is_seller=is_seller)
 
-	return render(request, 'register.html', context)
+	return render(request, 'register.html', {})
 
 def login_view(request, *args, **kwargs):
 	return render(request, 'login.html', {})
